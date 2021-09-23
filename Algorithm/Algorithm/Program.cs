5d1731df945c916af5f4143fc7ea5842233f6b1f
sionRyu <alpems15@sogang.ac.kr>
2021-09-23 14:29:42 +0900

    algorithmBP

---
+++ Algorithm/Algorithm/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Algorithm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Board board = new Board();
+            board.Initialize(25);
+
+            Console.CursorVisible = false;
+
+            const int WAIT_TICK = 1000 / 30;
+
+
+            int lastTick = 0;
+            while (true)
+            {
+                #region 프레임 관리
+                int currentTick = System.Environment.TickCount;
+                if ( currentTick - lastTick < WAIT_TICK)
+                {
+                    continue;
+                }
+                lastTick = currentTick;
+                #endregion
+
+
+
+
+                Console.SetCursorPosition(0, 0);
+                board.Render();
+            }
+        }
+    }
+}
---
+++ DataStructurePractice/DataStructurePractice/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DataStructurePractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+       
+        }
+    }
+}
